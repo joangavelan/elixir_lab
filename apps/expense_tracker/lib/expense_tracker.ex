45fc21f03be6e8c14d06b7dc821650e53a1e2d1b
joangavelan <joangavelan@gmail.com>
2024-11-17 03:32:05 -0500

    [expense_tracker] feat: add functionality to log expenses

--- apps/expense_tracker/lib/expense_tracker.ex
+++ apps/expense_tracker/lib/expense_tracker.ex
@@ -1,18 +1,46 @@
+defmodule Expense do
+  defstruct [:id, :amount, :category, :description, :date]
+end
+
 defmodule ExpenseTracker do
-  @moduledoc """
-  Documentation for `ExpenseTracker`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  # Client API
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
 
-  ## Examples
+  def log_expense(
+        amount,
+        category \\ "Uncategorized",
+        description \\ "",
+        date \\ Date.utc_today()
+      )
 
-      iex> ExpenseTracker.hello()
-      :world
+  def log_expense(amount, category, description, date) when is_number(amount) and amount > 0 do
+    GenServer.call(__MODULE__, {:log_expense, amount, category, description, date})
+  end
 
-  """
-  def hello do
-    :world
+  def log_expense(_amount, _category, _description, _date) do
+    {:error, :invalid_amount}
+  end
+
+  # Server callbacks
+  @impl true
+  def init(_) do
+    {:ok, []}
+  end
+
+  @impl true
+  def handle_call({:log_expense, amount, category, description, date}, _from, expenses) do
+    new_expense = %Expense{
+      id: :crypto.strong_rand_bytes(16) |> Base.encode16(),
+      amount: amount,
+      category: category,
+      description: description,
+      date: date
+    }
+
+    {:reply, {:ok, new_expense}, [new_expense | expenses]}
   end
 end
--- apps/expense_tracker/test/expense_tracker_test.exs
+++ apps/expense_tracker/test/expense_tracker_test.exs
@@ -2,7 +2,34 @@ defmodule ExpenseTrackerTest do
   use ExUnit.Case
   doctest ExpenseTracker
 
-  test "greets the world" do
-    assert ExpenseTracker.hello() == :world
+  setup do
+    {:ok, _pid} = ExpenseTracker.start_link([])
+    :ok
+  end
+
+  describe "log_expense/4" do
+    # Logging a valid expense
+    test "log a valid expense with all fields" do
+      assert {:ok, expense} =
+               ExpenseTracker.log_expense(50.75, "Food", "Lunch with friends", ~D[2024-11-14])
+
+      assert expense.amount == 50.75
+      assert expense.description == "Lunch with friends"
+      assert expense.category == "Food"
+      assert expense.date == ~D[2024-11-14]
+    end
+
+    # Logging an expense with default description, category and date
+    test "log an expense with default values for missing fields" do
+      assert {:ok, expense} = ExpenseTracker.log_expense(20.00)
+      assert expense.description == ""
+      assert expense.category == "Uncategorized"
+      assert expense.date == Date.utc_today()
+    end
+
+    # Handling invalid amount
+    test "reject logging an expense with invalid amount" do
+      assert {:error, :invalid_amount} = ExpenseTracker.log_expense(-10.00)
+    end
   end
 end
