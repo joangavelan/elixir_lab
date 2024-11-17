0be09b4129414df1cd52b8e293d3ce7b8e293da7
joangavelan <joangavelan@gmail.com>
2024-11-17 02:45:06 -0500

    [expense_tracker] build: initial commit

---
+++ apps/expense_tracker/lib/expense_tracker.ex
@@ -0,0 +1,18 @@
+defmodule ExpenseTracker do
+  @moduledoc """
+  Documentation for `ExpenseTracker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExpenseTracker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
